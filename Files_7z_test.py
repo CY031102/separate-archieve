3d203cb555508b50728dc4e13633162580612b83
Zep <52792159+CY031102@users.noreply.github.com>
2023-01-29 05:40:06 +0800

    Add files via upload

---
+++ Files_7z_test.py
@@ -0,0 +1,23 @@
+import subprocess
+import tkinter as tk
+from tkinter import filedialog
+import random
+import string
+
+root = tk.Tk()
+root.withdraw()
+
+#7zip location
+seven_zip = r"C:\Applications\7-Zip\7z.exe"
+
+def generate_password(length):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(length))
+
+files_to_compress = filedialog.askopenfilenames(
+    title="Select files to compress", 
+    filetypes=(("All files", "*.*"),))
+
+for file in files_to_compress:
+    password = generate_password(127)
+    subprocess.run([seven_zip, 'a', '-t7z', '-p'+password, file + '.7z', file])
+    print("File compressed successfully with password: ", password)
